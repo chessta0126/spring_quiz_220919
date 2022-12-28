ac605b3fa3760a8650c5b2ee61387101c7a9d844
test <test@test.com>
2022-12-28 18:42:21 +0900

    lesson04 Quiz01 완료

--- src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -2,12 +2,14 @@ package com.quiz.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.quiz.lesson04.bo.SellerBO;
+import com.quiz.lesson04.model.Seller;
 
 @RequestMapping("/lesson04/quiz01")
 @Controller
@@ -17,8 +19,9 @@ public class Lesson04Quiz01Controller {
 	private SellerBO sellerBO;
 	
 	// 입력창 주소
-	// http://localhost:8080/lesson04/quiz01/1
-	@RequestMapping(path="/1", method=RequestMethod.GET)
+	// http://localhost:8080/lesson04/quiz01/add_seller_view
+	@GetMapping("/add_seller_view") // 권장
+	// @RequestMapping(path="/add_seller_view", method=RequestMethod.GET)
 	public String addSellerView() {
 		// application.properties에서 설정한 앞뒤 경로 제외하고 쓰기
 		// 앞 : prefix=/WEB-INF/jsp/
@@ -31,11 +34,31 @@ public class Lesson04Quiz01Controller {
 	public String addSeller(
 			@RequestParam("nickname") String nickname
 			,@RequestParam(value="profileImageUrl", required=false) String profileImageUrl
-			,@RequestParam(value="temperature", required=false) double temperature
+			,@RequestParam(value="temperature", required=false) Double temperature // 래퍼클래스: null 가능
 			) {
 		
 		// DB insert
 		sellerBO.addSeller(nickname, profileImageUrl, temperature);
 		return "lesson04/after_add_seller"; // 결과 jsp
+	}
+	
+	// http://localhost:8080/lesson04/quiz01/seller_info?id=1
+	@GetMapping("/seller_info")
+	public String getLastUserView(
+			@RequestParam(value="id", required=false) Integer id
+			,Model model) {
+		// DB select - 가장 마지막에 추가된 사람 1명 가져오기
+		Seller seller = null;
+		if(id==null) {
+			seller = sellerBO.getLastSeller();
+		} else {
+			seller = sellerBO.getSellerById(id);
+		}
+		
+		// model에 데이터를 담아놓으면, jsp에서 꺼내서 쓸 수 있다.
+		model.addAttribute("seller",seller);
+		model.addAttribute("title","판매자 정보");
+		
+		return "lesson04/getLastSeller";
 	}
 }
